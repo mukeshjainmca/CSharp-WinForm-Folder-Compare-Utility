8483e9b686e0ccc8a4ceb1960d08a1f0bca0385c
mukeshjainmca <mukesh.300680@gmail.com>
2021-12-09 15:53:45 -0800

    Initial Commit

---
+++ FolderCompare/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace FolderCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Left Folder";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(92, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(479, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Right Folder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(92, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(479, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(577, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 22);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(577, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 22);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Compare";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Comparision Results";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FullName,
+            this.Status});
+            this.dataGridView1.Location = new System.Drawing.Point(9, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(643, 420);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // FullName
+            // 
+            this.FullName.DataPropertyName = "FullName";
+            this.FullName.HeaderText = "FullName";
+            this.FullName.Name = "FullName";
+            this.FullName.Width = 500;
+            // 
+            // Status
+            // 
+            this.Status.DataPropertyName = "Status";
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 542);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Folder Compare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FullName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+    }
+}
+
---
+++ FolderCompare/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderCompare
+{
+    public partial class Form1 : Form
+    {
+        private BindingSource bindingSource1 = new BindingSource();
+        private List<FileCompareResult> FileCompareResults = new List<FileCompareResult>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog2.SelectedPath;               
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+           
+            FileCompareResults.Clear();
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please Select Source Folder");
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Please Select Destination Folder");
+                return;
+            }
+
+            CompareFiles(textBox1.Text, textBox2.Text);
+            CompareDirectories(textBox1.Text, textBox2.Text);
+         
+            dataGridView1.DataSource = FileCompareResults;    
+            if(FileCompareResults.Count > 0)
+            {
+                dataGridView1.Columns[0].Width = 500;
+                dataGridView1.Columns[1].Width = 100;
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    row.HeaderCell.Value = (row.Index + 1).ToString();
+                }
+            }
+        }
+
+        private void CompareFiles(string srcPath,string destPath)
+        {
+            string[] fileEntriesSrc = new string[] { };
+            string[] fileEntriesdest = new string[] { };
+
+            if (!string.IsNullOrEmpty(srcPath))
+            {
+                fileEntriesSrc = Directory.GetFiles(srcPath);
+            }
+            if (!string.IsNullOrEmpty(destPath))
+            {
+                fileEntriesdest = Directory.GetFiles(destPath);
+            }
+            foreach (string filename in fileEntriesSrc)
+            {
+                string root = Path.GetFileName(filename);
+
+                if (root != null)
+                {
+                    if(!FileCompareResults.Any(l => l.FullName == filename.Replace(textBox1.Text + "\\", "")))
+                    FileCompareResults.Add(new FileCompareResult { FullName = filename.Replace(textBox1.Text + "\\",""), Status = "Left" });
+                }
+            }
+
+            foreach (string filename in fileEntriesdest)
+            {
+                string root = Path.GetFileName(filename);
+
+                if (root != null)
+                {
+                    var record = FileCompareResults.Where(l => l.FullName == filename.Replace(textBox2.Text + "\\", "")).FirstOrDefault();
+                    if (record != null && !string.IsNullOrEmpty(record.FullName))
+                    {
+                        record.Status = "Both";
+                    }
+                    else
+                    {
+                        if (!FileCompareResults.Any(l => l.FullName == filename.Replace(textBox2.Text + "\\", "")))
+                            FileCompareResults.Add(new FileCompareResult { FullName = filename.Replace(textBox2.Text + "\\", ""), Status = "Right" });
+                    }
+                }
+            }
+        }
+
+        private void CompareDirectories(string srcPath, string destPath)
+        {
+            string[] srcdirs = Directory.GetDirectories(srcPath, "*", SearchOption.AllDirectories);
+            string[] destcdirs = Directory.GetDirectories(destPath, "*", SearchOption.AllDirectories);
+
+            foreach (string dirname in srcdirs)
+            {
+                string root = Path.GetFileName(dirname);
+
+                if (root != null)
+                {
+                    FileCompareResults.Add(new FileCompareResult { FullName = root, Status = "Left" });
+                    CompareFiles(dirname, "");
+                }
+            }
+
+            foreach (string dirname in destcdirs)
+            {
+                string root = Path.GetFileName(dirname);
+
+                if (root != null)
+                {
+                    var record = FileCompareResults.Where(l => l.FullName == root).FirstOrDefault();
+                    if (record != null && !string.IsNullOrEmpty(record.FullName))
+                    {
+                        record.Status = "Both";
+                        CompareFiles(srcPath +"\\" + root, dirname);
+                    }
+                    else
+                    {
+                        FileCompareResults.Add(new FileCompareResult { FullName = root, Status = "Right" });
+                        CompareFiles("",dirname);
+                    }
+                }
+            }
+        }
+
+        public class FileCompareResult
+        {
+            public string FullName { get; set; }
+            public string Status { get; set; }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
